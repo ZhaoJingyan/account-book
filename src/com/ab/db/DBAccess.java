372c5d6b508bd78383289bdc81348989f064a066
Zhao Jinyan <949474952@qq.com>
2015-03-20 21:25:19 +0800

    数据库连接类

--- src/com/ab/db/DBAccess.java
+++ src/com/ab/db/DBAccess.java
@@ -1,5 +1,13 @@
 package com.ab.db;
 
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
 /**
  * 管理数据库连接。
  * 
@@ -7,5 +15,29 @@ package com.ab.db;
  *
  */
 public class DBAccess {
-
+	
+	/**
+	 * 构造方法。
+	 * 
+	 * @param xmlPath mybatis配置文件
+	 * @throws IOException 配置文件载入异常
+	 */
+	public DBAccess(String xmlPath) throws IOException{
+		Reader reader = Resources.getResourceAsReader(xmlPath);
+		sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+	}
+	
+	/**
+	 * 数据库连接工厂
+	 */
+	private SqlSessionFactory sqlSessionFactory = null;
+	
+	/**
+	 * 获取一个打开的数据库连接。
+	 * @return 数据库连接
+	 */
+	public SqlSession getSqlSession() {
+		return sqlSessionFactory.openSession();
+	}
+	
 }
